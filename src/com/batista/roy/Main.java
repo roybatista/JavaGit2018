aad75cf9eb995f22c8346212760685de2dbe6692
roybatista <roybatista@gmail.com>
2018-08-01 21:02:52 +0100

    add number

--- src/com/batista/roy/Main.java
+++ src/com/batista/roy/Main.java
@@ -3,5 +3,12 @@ package com.batista.roy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("JavaGit2018");
+        System.out.println(new Main().addNumbers(5,8));
+    }
+
+
+    private int addNumbers(int i, int j){
+        j = i + j;
+        return j;
     }
 }
